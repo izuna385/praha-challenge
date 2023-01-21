6abfc120639008101960004c656d9822ceb9f9bd
izuna385 <35322641+izuna385@users.noreply.github.com>
2023-01-22 07:03:12 +0900

    add deleted file

--- 1-DBモデリング1/init/sql/data/category.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO commerce.categories (id, name, created_at)
-VALUES 
-  (1, '盛り込み', '2020-03-15T09:00:00')
-, (2, 'にぎり', '2020-03-16T09:00:00')
-;
--- 1-DBモデリング1/init/sql/data/order.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO employee
-SET number    = 'DEV-0001'
-  , name      = '山田 太郎'
-  , email     = 'taro@example.com'
-  , role      = 'developer'
-  , position  = 'manager'
-  , hire_date = '2020-01-01'
-;
