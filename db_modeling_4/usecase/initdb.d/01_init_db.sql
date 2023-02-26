912faa3bd36cff05a7509b01ef6133316bc5d851
izuna385 <35322641+izuna385@users.noreply.github.com>
2023-02-26 09:04:45 +0900

    fix query

--- db_modeling_4/usecase/initdb.d/01_init_db.sql
+++ db_modeling_4/usecase/initdb.d/01_init_db.sql
@@ -14,38 +14,32 @@ CREATE TABLE `app`.`users` (
 );
 
 CREATE TABLE `app`.`publishers` (
-  `id` varchar(255) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) PRIMARY KEY NOT NULL,
   `reminder_id` varchar(255) NOT NULL
 );
 
 CREATE TABLE `app`.`recievers` (
-  `id` varchar(255) PRIMARY KEY NOT NULL,
-  `user_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) PRIMARY KEY NOT NULL,
   `reminder_id` varchar(255) NOT NULL
 );
 
 CREATE TABLE `app`.`reminders` (
-  `id` varchar(255) NOT NULL,
+  `id` varchar(255) PRIMARY KEY NOT NULL,
   `team_id` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL,
   `updated_at` timestamp NOT NULL,
-  `text` varcher NOT NULL,
-  `frequency` varcher NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `frequency` varchar(255) NOT NULL,
   `initial_remind_time` timestamp NOT NULL,
-  `next_Remind_time` timestamp NOT NULL,
+  `next_remind_time` timestamp NOT NULL,
   `publisher_id` varchar(255) NOT NULL,
   `reciever_id` varchar(255) NOT NULL
 );
 
 ALTER TABLE `app`.`users` ADD FOREIGN KEY (`team_id`) REFERENCES `app`.`teams` (`id`);
 
-ALTER TABLE `app`.`publishers` ADD FOREIGN KEY (`user_id`) REFERENCES `app`.`users` (`id`);
-
-ALTER TABLE `app`.`recievers` ADD FOREIGN KEY (`user_id`) REFERENCES `app`.`users` (`id`);
-
 ALTER TABLE `app`.`reminders` ADD FOREIGN KEY (`team_id`) REFERENCES `app`.`teams` (`id`);
 
-ALTER TABLE `app`.`reminders` ADD FOREIGN KEY (`publisher_id`) REFERENCES `app`.`publishers` (`id`);
+ALTER TABLE `app`.`reminders` ADD FOREIGN KEY (`publisher_id`) REFERENCES `app`.`publishers` (`user_id`);
 
-ALTER TABLE `app`.`reminders` ADD FOREIGN KEY (`reciever_id`) REFERENCES `app`.`recievers` (`id`);
+ALTER TABLE `app`.`reminders` ADD FOREIGN KEY (`reciever_id`) REFERENCES `app`.`recievers` (`user_id`);
--- db_modeling_4/usecase/initdb.d/02_insert_data.sql
+++ db_modeling_4/usecase/initdb.d/02_insert_data.sql
@@ -1,20 +1,36 @@
--- teamsテーブルにデータを挿入するためのクエリ
-INSERT INTO app.teams (id, name, access_token)
-VALUES ('1', 'Team A', 'abc123'), ('2', 'Team B', 'def456');
+-- Insert sample data for teams table
+INSERT INTO `app`.`teams` (`id`, `name`, `access_token`)
+VALUES
+('team1', 'Team 1', 'access_token1'),
+('team2', 'Team 2', 'access_token2');
 
--- usersテーブルにデータを挿入するためのクエリ
-INSERT INTO app.users (id, team_id, access_token)
-VALUES (1, '1', 'user_token_1'), (2, '1', 'user_token_2'), (3, '2', 'user_token_3');
+-- Insert sample data for users table
+INSERT INTO `app`.`users` (`id`, `team_id`, `access_token`)
+VALUES
+(1, 'team1', 'access_token1_user1'),
+(2, 'team1', 'access_token1_user2'),
+(3, 'team2', 'access_token2_user1'),
+(4, 'team2', 'access_token2_user2');
 
--- publishersテーブルにデータを挿入するためのクエリ
-INSERT INTO app.publishers (id, user_id, reminder_id)
-VALUES ('pub1', '1', 'rem1'), ('pub2', '2', 'rem2');
+-- Insert sample data for publishers table
+INSERT INTO `app`.`publishers` (`user_id`, `reminder_id`)
+VALUES
+('1', '7e8f9de1-afdb-4a43-b37e-dbf844376711'),
+('2', 'eeff2d0d-3179-4a7d-a21a-0e7e172df45b'),
+('3', 'a0aa4a22-4d9a-4c5d-a620-6fcd5b5e47df'),
+('4', '94a6b25f-80ce-413c-a59b-47a6b8127346');
 
--- recieversテーブルにデータを挿入するためのクエリ
-INSERT INTO app.recievers (id, user_id, reminder_id)
-VALUES ('rec1', '1', 'rem1'), ('rec2', '2', 'rem2'), ('rec3', '3', 'rem2');
+-- Insert sample data for recievers table
+INSERT INTO `app`.`recievers` (`user_id`, `reminder_id`)
+VALUES
+('1', '7e8f9de1-afdb-4a43-b37e-dbf844376711'),
+('2', '7e8f9de1-afdb-4a43-b37e-dbf844376711'),
+('3', 'eeff2d0d-3179-4a7d-a21a-0e7e172df45b'),
+('4', 'a0aa4a22-4d9a-4c5d-a620-6fcd5b5e47df');
 
--- remindersテーブルにデータを挿入するためのクエリ
-INSERT INTO app.reminders (id, team_id, created_at, updated_at, text, frequency, initial_remind_time, next_Remind_time, publisher_id, reciever_id)
-VALUES ('rem1', '1', NOW(), NOW(), 'Reminder 1', 'daily', NOW(), NOW() + INTERVAL 1 DAY, 'pub1', 'rec1'),
-('rem2', '1', NOW(), NOW(), 'Reminder 2', 'weekly', NOW(), NOW() + INTERVAL 1 WEEK, 'pub2', 'rec2');
+-- Insert sample data for reminders table
+INSERT INTO `app`.`reminders` (`id`, `team_id`, `created_at`, `updated_at`, `text`, `frequency`, `initial_remind_time`, `next_remind_time`, `publisher_id`, `reciever_id`)
+VALUES
+('7e8f9de1-afdb-4a43-b37e-dbf844376711', 'team1', NOW(), NOW(), 'Reminder 1', 'daily', NOW(), NOW(), '1', '1'),
+('eeff2d0d-3179-4a7d-a21a-0e7e172df45b', 'team1', NOW(), NOW(), 'Reminder 2', 'weekly', NOW(), NOW(), '2', '3'),
+('a0aa4a22-4d9a-4c5d-a620-6fcd5b5e47df', 'team2', NOW(), NOW(), 'Reminder 3', 'monthly', NOW(), NOW(), '4', '4');
